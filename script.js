86ee0d9a398c5f49f2559f21b7e0556a081be99d
Victor-Aken <akenvictor01@gmail.com>
2022-06-14 13:51:27 +0100

    Added delete button functionality

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let addToCartButton = document.querySelectorAll("#add-btn");
 let parentItemContainer = document.getElementsByTagName("tbody")[0];
 let itemCount = document.getElementById("itemCount");
 let deleteButton = document.getElementsByClassName('delete-button');
+let itemQuantity = document.getElementsByName('quantity')[0];
+let itemRows = parentItemContainer.getElementsByTagName('tr');
+
 
 for (let i = 0; i < addToCartButton.length; i++ ){
     
-    addToCartButton[i].addEventListener( 'click', function(){
+    addToCartButton[i].addEventListener( 'click', function(event){
 
       
         let btn = event.target;
@@ -16,44 +19,52 @@ for (let i = 0; i < addToCartButton.length; i++ ){
         let ProductPrice = btn_parent.children[3].innerText;
         let ProductImage = btn_grandparent.children[0].src;
 
-
-
         let itemContainer = document.createElement('tr');
+        
 
-        itemContainer.innerHTML = ` <td><img class="item-img" src="${ProductImage}" alt="" class="product-img"></td>
+        itemContainer.innerHTML = `<td><img class="item-img" src="${ProductImage}" alt="" class="product-img"></td>
         <td>${ProductName}</td>
-        <td><input type="number" name="quantity" id="quantity"></td>
-        <td>${ ProductPrice}</td>
+        <td><input type="number" name="quantity" id="quantity" value="1"></td>
+        <td class="price">${ ProductPrice}</td>
         <td><button class="delete-button"><img class="delete-icon" src="shopping cart img/delete-icon.png" alt=""></button></td>`
 
         parentItemContainer.append(itemContainer);
         itemCount.innerText = parentItemContainer.children.length;
 
-        for( let i = 0; i < deleteButton; i++ ){
+        for( let i = 0; i < deleteButton.length; i++ ){
             deleteButton[i].addEventListener( 'click', removeItem )
         }
-    
 
-        // for( let i = 0; i < itemQuantity; i++ ){
-        //     itemQuantity[i].addEventListener( 'change', function(event){
+        // updateGrandTotal();
 
-        //         let number_of_item = event.target;
-        //         console.log(23)
-
-    
-        //     })
-        // }
-        
     })
 }
 
 
 function removeItem(event){
     removeBtn = event.target;
-    removeBtn_grandparent = removeBtn.parentElement.parentElement;
+    removeBtn_grandparent = removeBtn.parentElement.parentElement.parentElement;
 
     removeBtn_grandparent.remove();
-    // itemContainer.remove();
+    // updateGrandTotal();
 }
+
+// function updateGrandTotal(){
+
+//     let total = 0;
+
+//     for ( let i = 0; i < itemRows.length; i++ ){
+//         particularItemRow = itemRows[i];
+//         let priceElement = particularItemRow.getElementsByClassName('price')[0];
+//         let quantityElement = particularItemRow.querySelectorAll('#quantity')[0];
+//         let price = priceElement.innerText.replace('$', '');
+//         let quantityvalue = quantityElement.value;
+
+//         total = total + (price * quantityvalue);
+//     }
+
+//     grandTotal = document.getElementById('grand-total').innerText = '$' + total;
+// }
+
 
 
